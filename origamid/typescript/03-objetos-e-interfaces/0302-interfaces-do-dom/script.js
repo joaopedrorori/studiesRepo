b2887d7f85d74bfd02ceab597abd40dfd50cdbca
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-21 16:09:34 -0300

    03 objetos e interfaces pronto

--- origamid/typescript/03-objetos-e-interfaces/0302-interfaces-do-dom/script.js
+++ origamid/typescript/03-objetos-e-interfaces/0302-interfaces-do-dom/script.js
@@ -1,1 +1,39 @@
 "use strict";
+// interfaces do DOM
+// querySelector
+// Quando selecionamos um elemento do DOM com o querySelector,
+//  o objeto retornado dependera da string que passamos no metodo
+// https://developer.mozilla.org/en-US/docs/Web/API
+// const video = document.querySelector("video");
+// if (video instanceof HTMLVideoElement) console.log(video.volume);
+// querySelectorAll
+// Este retorna uma NodeList de elementos.
+// Nao confundir o nome da interface NodeListOf com o nome da classe NodeList
+// const links = document.querySelectorAll(".link");
+// console.log(links instanceof NodeList);
+// links.forEach((link) => {
+//   if (link instanceof HTMLAnchorElement) {
+//     console.log(link.href);
+//   } else {
+//     console.log(typeof link);
+//   }
+// });
+// const anchorLinks = links.filter((link) => link instanceof HTMLAnchorElement);
+// erro, filter é um método de Array e não de NodeList
+// Funciona, pq a NodeList foi transformada em uma Array
+// const anchorLinks = Array.from(links).filter(
+//   (link) => link instanceof HTMLAnchorElement
+// );
+// Exercício
+// 1 - Selecione os elementos com a classe link.
+// 2 - Crie uma função que deve ser executada para cada elemento.
+// 3 - Modificar através da função o estilo da color e border.
+const links = document.querySelectorAll(".link");
+links === null || links === void 0 ? void 0 : links.forEach((link) => {
+    if (link instanceof HTMLElement)
+        changeColor(link);
+});
+function changeColor(item) {
+    item.style.color = "red";
+    item.style.border = "2px solid green";
+}
