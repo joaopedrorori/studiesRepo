a056b90793ce06a0d9b29e148c743ef04b15cbec
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-23 19:42:22 -0300

    0307 unknown pronto

--- origamid/typescript/03-objetos-e-interfaces/0307-unknown/script.js
+++ origamid/typescript/03-objetos-e-interfaces/0307-unknown/script.js
@@ -1,1 +1,17 @@
 "use strict";
+// Unknown
+// Indica que nao sabemos o tipo de dados que pode ser passado.
+// Diferente do any, o unknown so ira permitir o uso de metodos quando a Type Safety estiver garantida
+// Any x Unknown
+// Unknown nao vai assumir nada que o tipo pode ser, entao pra poder usar qualquer metodo ou propriedade, uma typeguard deve ser usada
+function typeGuard(value) {
+    if (typeof value === "string")
+        return value.toUpperCase();
+    if (typeof value === "number")
+        return value.toFixed();
+    if (value instanceof HTMLElement)
+        return value.innerText;
+}
+console.log(typeGuard("string"));
+console.log(typeGuard(200));
+console.log(typeGuard(document.body));
