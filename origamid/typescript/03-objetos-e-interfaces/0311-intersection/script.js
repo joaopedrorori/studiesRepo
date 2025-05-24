884abcf29f7cd5814a63bb97838b1a60de5e191a
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-23 23:03:06 -0300

    0311 intersection

--- origamid/typescript/03-objetos-e-interfaces/0311-intersection/script.js
+++ origamid/typescript/03-objetos-e-interfaces/0311-intersection/script.js
@@ -1,1 +1,16 @@
 "use strict";
+// Intersection
+// Funciona em parte como o extends para interfaces, mas pode ser utilizado em Types
+function handleProdutoCarro(dados) {
+    dados.rodas;
+    dados.portas;
+    dados.preco;
+}
+handleProdutoCarro({
+    preco: 20000,
+    rodas: 4,
+    portas: 5,
+});
+function handleInterfaceCarro(carro) { }
+window.userId = 200;
+console.log(window.userId);
