85dec9853db795628484282e1397a203e3d4bad6
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-27 18:36:51 -0300

    0504 bibliotecas externas finalizado

--- origamid/typescript/05-desenvolvimento/0504-bibliotecas-externas/dist/script.js
+++ origamid/typescript/05-desenvolvimento/0504-bibliotecas-externas/dist/script.js
@@ -1,3 +1,6 @@
 "use strict";
-console.log(true);
+const body = $("body");
+const iframe = document.getElementById("vimeo");
+const player = new Vimeo.Player(iframe);
+console.log(player);
 //# sourceMappingURL=script.js.map
--- origamid/typescript/05-desenvolvimento/0504-bibliotecas-externas/src/script.ts
+++ origamid/typescript/05-desenvolvimento/0504-bibliotecas-externas/src/script.ts
@@ -1,1 +1,9 @@
-console.log(true);
+const body = $("body");
+// console.log(body.addClass("ativo"));
+
+// console.log(_.difference([1, 2, 3, 5, 6], [2, 3, 1, 9]));
+
+declare const Vimeo: any;
+const iframe = document.getElementById("vimeo");
+const player = new Vimeo.Player(iframe);
+console.log(player);
