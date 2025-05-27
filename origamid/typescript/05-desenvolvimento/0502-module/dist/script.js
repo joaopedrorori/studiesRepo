69253a5c007c2a8b08b44cf8bb550a8b4cabd1c0
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-27 11:47:15 -0300

    0502 module feito

--- origamid/typescript/05-desenvolvimento/0502-module/dist/script.js
+++ origamid/typescript/05-desenvolvimento/0502-module/dist/script.js
@@ -1,3 +1,9 @@
-"use strict";
-console.log(true);
+import { URL_BASE } from "./global.js";
+import pluginSlide from "./pluginSlide.js";
+pluginSlide("div");
+console.log(URL_BASE);
+const livro = {
+    nome: "str",
+    preco: 0,
+};
 //# sourceMappingURL=script.js.map
