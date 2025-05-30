68d67230842d17db227f90c91fbb32ffc62c6700
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-30 09:17:41 -0300

    alterando frases para ingles

--- origamid/typescript/06-pratica-dados/0601-manipulacao-de-dados/dist/script.js
+++ origamid/typescript/06-pratica-dados/0601-manipulacao-de-dados/dist/script.js
@@ -101,9 +101,20 @@ function table(data) {
         <tr>
           <td>${item.Nome}</td>
           <td>${item.Email}</td>
-          <td>${item["Valor (R$)"]}</td>
-          <td>${item["Forma de Pagamento"]}</td>
-          <td>${item.Status}</td>
+          <td>${item["Valor (R$)"] !== "-"
+                ? Number(item["Valor (R$)"].replace(".", "").replace(",", ".")).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                })
+                : "-"}</td>
+          <td>${item["Forma de Pagamento"] === "Cartão de Crédito"
+                ? "Credit Card"
+                : "Ticket"}</td>
+          <td>${item.Status === "Paga"
+                ? "Paid"
+                : item.Status === "Recusada pela operadora de cartão"
+                    ? "Refused by the credit card company"
+                    : "Waiting payment"}</td>
         </tr>
       `;
         }
