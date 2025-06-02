154f317e85817eba5c92f0ccfb2272865317747d
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-02 16:32:46 -0300

    moeda normalizada

---
+++ origamid/typescript/06-pratica-dados/correcao/dist/moedaParaNumero.js
@@ -0,0 +1,5 @@
+export default function moedaParaNumero(moeda) {
+    const numero = Number(moeda.replaceAll(".", "").replaceAll(",", "."));
+    return isNaN(numero) ? null : numero;
+}
+//# sourceMappingURL=moedaParaNumero.js.map
