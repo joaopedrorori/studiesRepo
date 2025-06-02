b7087bfb6b2692d4e6ee36bbe2d3aac449a6918c
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-02 19:16:34 -0300

    funcionalidade buttonPrev e buttonNext adicionadas

--- origamid/typescript/07-pratica-dom/src/Slide.ts
+++ origamid/typescript/07-pratica-dom/src/Slide.ts
@@ -19,17 +19,41 @@ export default class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
 
   hide(el: Element) {
     el.classList.remove("active");
   }
-
   show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Previous Slide";
+    nextButton.innerText = "Next Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    nextButton.addEventListener("pointerup", () => this.next());
+    prevButton.addEventListener("pointerup", () => this.prev());
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
