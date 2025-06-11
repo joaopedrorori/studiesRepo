9a466762546cfbf7c227932e31b5988335d3c379
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-11 15:46:02 -0300

    02-estrutura de dados do augusto galego pronto

---
+++ dsa-augusto-galego/02-estruturas-de-dados/0211-b-tree/script.js
@@ -0,0 +1,5 @@
+// Uma arvore autobalanceavel que vai seguir regras
+// Primeiro sao definidas o numero de chaves e o numero de keys da b-tree
+// O numero de children sempre vai ser 1 a mais do que o numero de keys
+// Todos as ultimas "folhas" da b-tree vao estar no mesmo nivel
+// https://cuuduongthancong.com/~galles/visualization/BTree.html?
