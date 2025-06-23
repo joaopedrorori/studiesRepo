fba8f92e7f127758a7c3d8966c81bc14c0faf6ef
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-23 14:31:43 -0300

    aula 2 finalizada

--- origamid/react-com-typescript/04-projeto-tsx/src/App.tsx
+++ origamid/react-com-typescript/04-projeto-tsx/src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Style.css";
+import Resumo from "./Pages/Resumo";
+import Sidenav from "./Components/Sidenav";
+import Header from "./Components/Header";
 
 function App() {
-  return <div>app</div>;
+  return (
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
+    </div>
+  );
 }
 
 export default App;
