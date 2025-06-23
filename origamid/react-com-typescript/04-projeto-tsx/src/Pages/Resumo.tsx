0618e78006fced4b36a697f7fcd856d7108ee505
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-23 16:26:27 -0300

    parte inicial resumo feita

--- origamid/react-com-typescript/04-projeto-tsx/src/Pages/Resumo.tsx
+++ origamid/react-com-typescript/04-projeto-tsx/src/Pages/Resumo.tsx
@@ -1,11 +1,45 @@
-import React from "react";
 import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
   const { data } = useData();
 
-  // console.log(data);
-  return <div>Resumo</div>;
+  if (data === null) return;
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status !== "falha")
+              .reduce((acc, curr) => acc + curr.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "pago")
+              .reduce((acc, curr) => acc + curr.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "processando")
+              .reduce((acc, curr) => acc + curr.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Graficos</div>
+    </section>
+  );
 };
 
 export default Resumo;
