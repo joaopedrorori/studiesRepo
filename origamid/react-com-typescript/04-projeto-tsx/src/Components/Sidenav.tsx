80fdf0691b443af5ea28c1010c5dc09d53a4d5a3
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-23 16:52:00 -0300

    sidenav feita

--- origamid/react-com-typescript/04-projeto-tsx/src/Components/Sidenav.tsx
+++ origamid/react-com-typescript/04-projeto-tsx/src/Components/Sidenav.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import resumo from "../assets/icons/resumo.svg";
+import vendas from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import configuracoes from "../assets/icons/configuracoes.svg";
+import contato from "../assets/icons/contato.svg";
+import sair from "../assets/icons/sair.svg";
+import FintechSVG from "../assets/FintechSVG";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} />
+          </span>
+          <a>Configuracoes</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
