8051ef159bc24f7250eeaed96494753235e6b138
joaopedrorori <joaopedrorr.df@gmail.com>
2025-06-23 13:10:15 -0300

    cap 2 comecado

---
+++ js-book/cap02/js/ex2_2.js
@@ -0,0 +1,16 @@
+// const titleInput = document.querySelector("#title-input");
+// const minutesInput = document.querySelector("#duration-input");
+
+// const titleOut = document.querySelector("#film-title");
+// const timeOut = document.querySelector("#film-duration");
+
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   const horas = Math.floor(minutesInput.value / 60);
+//   const minutes = minutesInput.value % 60;
+//   titleOut.innerText = titleInput.value;
+//   if (horas > 1) timeOut.innerText = `${horas} horas e ${minutes} minutos`;
+//   else timeOut.innerText = `${horas} hora e ${minutes} minutos`;
+// });
